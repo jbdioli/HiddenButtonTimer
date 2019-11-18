411495848762faeff25457f04c929dd588b854cb
jbdioli <jdeoworld@gmail.com>
2019-11-18 16:06:00 +0100

    add

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  flag = true;
+
   constructor() {}
+
+
+  timer() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 2000);
+    });
+  }
+
+  async locationTime() {
+    await this.timer();
+  }
+
+  onClick() {
+    this.flag = false;
+    this.locationTime().then(() => {this.flag = true; });
+
+  }
 
 }
